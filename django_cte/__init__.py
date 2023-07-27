e6c7f8e30853cc80eddf6d0b714f1c9dc877c112
Paul Gammans <pgammans@gammascience.co.uk>
2023-07-27 09:47:21 +0100

    add pyproject.toml and tox to make testing simpler

--- django_cte/__init__.py
+++ django_cte/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from .cte import CTEManager, CTEQuerySet, With  # noqa
-
-__version__ = "1.3.1"
+from .cte import CTEManager, CTEQuerySet, With  # noqa: F401
+from ._version import version as __version__  # noqa: F401
