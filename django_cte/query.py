7929c8c80e351035dd922c4492c9d992bb65743e
Daniel Miller <dmiller@dimagi.com>
2025-06-05 11:47:02 -0400

    Dynamically create CTE compiler classes
    This commit may be easier to view by ignoring whitespace changes.

--- django_cte/query.py
+++ django_cte/query.py
@@ -2,11 +2,6 @@ import django
 from django.core.exceptions import EmptyResultSet
 from django.db import connections
 from django.db.models.sql import DeleteQuery, Query, UpdateQuery
-from django.db.models.sql.compiler import (
-    SQLCompiler,
-    SQLDeleteCompiler,
-    SQLUpdateCompiler,
-)
 from django.db.models.sql.constants import LOUTER
 
 from .join import QJoin
@@ -44,8 +39,10 @@ class CTEQuery(Query):
             raise ValueError("Need either using or connection")
         if using:
             connection = connections[using]
-        klass = COMPILER_TYPES.get(self.__class__, CTEQueryCompiler)
-        return klass(self, connection, using, *args, **kwargs)
+
+        compiler_class = connection.ops.compiler(self.compiler)
+        cte_compiler = mixin_class(CTECompiler, compiler_class)
+        return cte_compiler(self, connection, using, *args, **kwargs)
 
     def __chain(self, _name, klass=None, *args, **kwargs):
         klass = QUERY_TYPES.get(klass, self.__class__)
@@ -57,78 +54,75 @@ class CTEQuery(Query):
         return self.__chain("chain", klass)
 
 
-class CTECompiler:
+def generate_cte_sql(connection, query, as_sql):
+    if not query._with_ctes:
+        return as_sql()
 
-    @classmethod
-    def generate_sql(cls, connection, query, as_sql):
-        if not query._with_ctes:
-            return as_sql()
+    ctes = []
+    params = []
+    for cte in query._with_ctes:
+        if django.VERSION > (4, 2):
+            _ignore_with_col_aliases(cte.query)
 
-        ctes = []
-        params = []
-        for cte in query._with_ctes:
-            if django.VERSION > (4, 2):
-                _ignore_with_col_aliases(cte.query)
+        alias = query.alias_map.get(cte.name)
+        should_elide_empty = (
+                not isinstance(alias, QJoin) or alias.join_type != LOUTER
+        )
 
-            alias = query.alias_map.get(cte.name)
-            should_elide_empty = (
-                    not isinstance(alias, QJoin) or alias.join_type != LOUTER
+        compiler = cte.query.get_compiler(
+            connection=connection, elide_empty=should_elide_empty
+        )
+
+        qn = compiler.quote_name_unless_alias
+        try:
+            cte_sql, cte_params = compiler.as_sql()
+        except EmptyResultSet:
+            # If the CTE raises an EmptyResultSet the SqlCompiler still
+            # needs to know the information about this base compiler
+            # like, col_count and klass_info.
+            as_sql()
+            raise
+        template = get_cte_query_template(cte)
+        ctes.append(template.format(name=qn(cte.name), query=cte_sql))
+        params.extend(cte_params)
+
+    explain_attribute = "explain_info"
+    explain_info = getattr(query, explain_attribute, None)
+    explain_format = getattr(explain_info, "format", None)
+    explain_options = getattr(explain_info, "options", {})
+
+    explain_query_or_info = getattr(query, explain_attribute, None)
+    sql = []
+    if explain_query_or_info:
+        sql.append(
+            connection.ops.explain_query_prefix(
+                explain_format,
+                **explain_options
             )
+        )
+        # this needs to get set to None so that the base as_sql() doesn't
+        # insert the EXPLAIN statement where it would end up between the
+        # WITH ... clause and the final SELECT
+        setattr(query, explain_attribute, None)
 
-            compiler = cte.query.get_compiler(
-                connection=connection, elide_empty=should_elide_empty
-            )
+    if ctes:
+        # Always use WITH RECURSIVE
+        # https://www.postgresql.org/message-id/13122.1339829536%40sss.pgh.pa.us
+        sql.extend(["WITH RECURSIVE", ", ".join(ctes)])
+    base_sql, base_params = as_sql()
 
-            qn = compiler.quote_name_unless_alias
-            try:
-                cte_sql, cte_params = compiler.as_sql()
-            except EmptyResultSet:
-                # If the CTE raises an EmptyResultSet the SqlCompiler still
-                # needs to know the information about this base compiler
-                # like, col_count and klass_info.
-                as_sql()
-                raise
-            template = cls.get_cte_query_template(cte)
-            ctes.append(template.format(name=qn(cte.name), query=cte_sql))
-            params.extend(cte_params)
+    if explain_query_or_info:
+        setattr(query, explain_attribute, explain_query_or_info)
 
-        explain_attribute = "explain_info"
-        explain_info = getattr(query, explain_attribute, None)
-        explain_format = getattr(explain_info, "format", None)
-        explain_options = getattr(explain_info, "options", {})
+    sql.append(base_sql)
+    params.extend(base_params)
+    return " ".join(sql), tuple(params)
 
-        explain_query_or_info = getattr(query, explain_attribute, None)
-        sql = []
-        if explain_query_or_info:
-            sql.append(
-                connection.ops.explain_query_prefix(
-                    explain_format,
-                    **explain_options
-                )
-            )
-            # this needs to get set to None so that the base as_sql() doesn't
-            # insert the EXPLAIN statement where it would end up between the
-            # WITH ... clause and the final SELECT
-            setattr(query, explain_attribute, None)
 
-        if ctes:
-            # Always use WITH RECURSIVE
-            # https://www.postgresql.org/message-id/13122.1339829536%40sss.pgh.pa.us
-            sql.extend(["WITH RECURSIVE", ", ".join(ctes)])
-        base_sql, base_params = as_sql()
-
-        if explain_query_or_info:
-            setattr(query, explain_attribute, explain_query_or_info)
-
-        sql.append(base_sql)
-        params.extend(base_params)
-        return " ".join(sql), tuple(params)
-
-    @classmethod
-    def get_cte_query_template(cls, cte):
-        if cte.materialized:
-            return "{name} AS MATERIALIZED ({query})"
-        return "{name} AS ({query})"
+def get_cte_query_template(cte):
+    if cte.materialized:
+        return "{name} AS MATERIALIZED ({query})"
+    return "{name} AS ({query})"
 
 
 class CTEUpdateQuery(UpdateQuery, CTEQuery):
@@ -136,6 +130,10 @@ class CTEUpdateQuery(UpdateQuery, CTEQuery):
 
 
 class CTEDeleteQuery(DeleteQuery, CTEQuery):
+    # NOTE: it is currently not possible to execute delete queries that
+    # reference CTEs without patching `QuerySet.delete` (Django method)
+    # to call `self.query.chain(sql.DeleteQuery)` instead of
+    # `sql.DeleteQuery(self.model)`
     pass
 
 
@@ -152,12 +150,12 @@ def _ignore_with_col_aliases(cte_query):
             query.ignore_with_col_aliases = True
 
 
-class CTEQueryCompiler(SQLCompiler):
+class CTECompiler:  # mixin for django.db.models.sql.compiler.SQLCompiler
 
     def as_sql(self, *args, **kwargs):
         def _as_sql():
-            return super(CTEQueryCompiler, self).as_sql(*args, **kwargs)
-        return CTECompiler.generate_sql(self.connection, self.query, _as_sql)
+            return super(CTECompiler, self).as_sql(*args, **kwargs)
+        return generate_cte_sql(self.connection, self.query, _as_sql)
 
     def get_select(self, **kw):
         if kw.get("with_col_aliases") \
@@ -166,28 +164,14 @@ class CTEQueryCompiler(SQLCompiler):
         return super().get_select(**kw)
 
 
-class CTEUpdateQueryCompiler(SQLUpdateCompiler):
-
-    def as_sql(self, *args, **kwargs):
-        def _as_sql():
-            return super(CTEUpdateQueryCompiler, self).as_sql(*args, **kwargs)
-        return CTECompiler.generate_sql(self.connection, self.query, _as_sql)
-
-
-class CTEDeleteQueryCompiler(SQLDeleteCompiler):
-
-    # NOTE: it is currently not possible to execute delete queries that
-    # reference CTEs without patching `QuerySet.delete` (Django method)
-    # to call `self.query.chain(sql.DeleteQuery)` instead of
-    # `sql.DeleteQuery(self.model)`
-
-    def as_sql(self, *args, **kwargs):
-        def _as_sql():
-            return super(CTEDeleteQueryCompiler, self).as_sql(*args, **kwargs)
-        return CTECompiler.generate_sql(self.connection, self.query, _as_sql)
+def mixin_class(mixin, base_class):
+    if issubclass(base_class, mixin):
+        return base_class
+    mixed = _mixin_cache.get(base_class)
+    if mixed is None:
+        name = f"{mixin.__name__}{base_class.__name__}"
+        mixed = _mixin_cache[base_class] = type(name, (mixin, base_class), {})
+    return mixed
 
 
-COMPILER_TYPES = {
-    CTEUpdateQuery: CTEUpdateQueryCompiler,
-    CTEDeleteQuery: CTEDeleteQueryCompiler,
-}
+_mixin_cache = {}
